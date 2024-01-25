6b01f8a9a54a08caf638006a83be9825ac434059
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-25 20:14:54 +0530

    Day - 3

--- Day - 3 Deleting an element in array/script.js
+++ Day - 3 Deleting an element in array/script.js
@@ -6,3 +6,7 @@ for (let i = pos; i < data.length - 1; i++) {
 }
 data.length = data.length - 1;
 console.log(data);
+
+//default function 
+data.splice(2, 1) // here 2 means 3rd pos and 1 means to delete only one element
+console.log(data);
