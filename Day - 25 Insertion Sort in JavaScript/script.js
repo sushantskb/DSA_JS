27740f92f54161ea3e1f007dd0d246a86e7dd505
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-02-16 00:14:58 +0530

    Day - 25

---
+++ Day - 25 Insertion Sort in JavaScript/script.js
@@ -0,0 +1,15 @@
+let data = [12, 11, 13, 5, 6];
+function insertionSort(data){
+    let i, current, j;
+    for (i = 1; i < data.length; i++) {
+        current = data[i];
+        j = i - 1;
+        while(j >= 0 && data[j] > current){
+            data[j + 1] = data[j];
+            j = j - 1; 
+        }
+        data[j + 1] = current;
+    }
+}
+insertionSort(data);
+console.log(data);
