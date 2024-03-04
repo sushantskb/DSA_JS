fd13a013f1bcbe4cedaf2088a57ef07f08c81688
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-03-05 00:55:42 +0530

    Day - 40

---
+++ Day - 40 Search Node in Binary Search Tree/script.js
@@ -0,0 +1,72 @@
+const prompt = require("prompt-sync")();
+class Node {
+    constructor(val){
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null;
+    }
+
+    makeTree(val){
+        let newNode = new Node(val);
+        if(this.root === null){
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(root, newNode){
+        if(root.value > newNode.value){
+            if(root.left === null){
+                root.left = newNode;
+            } else {
+                this.insertNode(root.left, newNode);
+            }
+        } else {
+            if(root.right == null) {
+                root.right = newNode;
+            } else {
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
+
+    search(root, val){
+        if(root === null){
+            return false;
+        } else if(root.value === val){
+            return  true;
+        } else if(root.value > val){
+            return this.search(root.left, val);
+        } else {
+            return this.search(root.right, val);
+        }
+    }
+
+    isTreeEmpty(){
+        return this.root === null;
+    }
+}
+
+let bst = new BST();
+
+bst.makeTree(50);  // Root node
+bst.makeTree(20);   // Left subtree
+bst.makeTree(80);     // Right subtree
+bst.makeTree(10);      // Left-most subtree
+bst.makeTree(60);       // Right-most subtree
+// console.log(bst);
+
+let result = prompt("Enter the number: ");
+
+if(bst.search(bst.root, result)){
+    console.log("Found");
+} else {
+    console.log("Not Found");
+}
