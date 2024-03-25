943a1194c767e992de08ef13e50c1b6461f7eaeb
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-03-25 18:13:52 +0530

    Day-45

---
+++ Day - 45 Graph Adjancy Matrix/script.js
@@ -0,0 +1,11 @@
+let data = [
+  //       A  B  C  D
+  /* A */ [0, 1, 1, 0],
+  /* B */ [1, 0, 1, 1],
+  /* C */ [0, 1, 0, 0],
+  /* D */ [0, 1, 0, 0],
+];
+
+// lets check the which points are connected
+console.log(data[2][1]);  /* C and B */
+console.log(data[0][3]); /* A and D */
